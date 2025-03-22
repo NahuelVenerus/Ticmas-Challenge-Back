ebaebaad0875872e6ad37d070bc847eb276af7f3
Nahuel Venerus <nahuelvenerus@gmail.com>
2025-03-22 15:08:26 -0300

    added cors settings

--- src/config/main.ts
+++ src/config/main.ts
@@ -6,6 +6,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Task Manager API')
     .setDescription('Documentation for Task Manager API')
@@ -17,6 +23,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   console.log(`App running on PORT: ${process.env.DB_PORT}`);
   await app.listen(process.env.DB_PORT ?? 3000);
 }
